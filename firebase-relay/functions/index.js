736fa951656aba7a3fc32787d5ff0950af12def9
HoshinoSubaru <subaru@urlounge.co.jp>
2025-05-13 09:00:28 +0000

    SU：FireBase途中

--- firebase-relay/functions/index.js
+++ firebase-relay/functions/index.js
@@ -2,10 +2,13 @@ const functions = require("firebase-functions");
 const fetch = require("node-fetch");
 
 exports.relayToGAS = functions.https.onRequest(async (req, res) => {
-  const { uid, name, page } = req.body;
+  const {uid, name, page} = req.body;
 
   const gasUrl = "https://script.google.com/a/macros/urlounge.co.jp/s/AKfycbxVP33NxiZRi_rIXzXRaN5-wNmVEVeFCQzPXnDdM4oxaF6MzuILwkDwNMEKTNm8V11Tdw/exec";
-  const fullUrl = `${gasUrl}?uid=${encodeURIComponent(uid)}&name=${encodeURIComponent(name)}&page=${encodeURIComponent(page)}`;
+  const fullUrl = `${gasUrl}?uid=${encodeURIComponent(uid)}` +
+                `&name=${encodeURIComponent(name)}` +
+                `&page=${encodeURIComponent(page)}`;
+
 
   try {
     const response = await fetch(fullUrl);
